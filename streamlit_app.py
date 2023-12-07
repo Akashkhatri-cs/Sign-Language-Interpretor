a334da3eb5de7ca58ea879d069910f5ea8115f21
Akashkhatri-cs <85150899+Akashkhatri-cs@users.noreply.github.com>
2023-12-06 22:55:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
 if uploaded_file is not None:
     if st.button('Predict'):
         files = {'file': uploaded_file.getvalue()}
-        response = requests.post('http://74.12.105.219:8000/predict', files=files)
+        response = requests.post('http://74.12.105.219:8090/predict', files=files)
         result = response.json()
         st.write('Predicted Letter: ', result['predicted_letter'])
